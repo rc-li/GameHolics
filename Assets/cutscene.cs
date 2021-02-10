8cbbb1beb95fa75be2fc5eadf9b4d35eadba08d6
YaqiHan <hanyaqi141@gmail.com>
2021-02-09 22:36:29 -0800

    Initial Cutscene Design
    Initial Cutscene Design with text box play but no image play

---
+++ Assets/cutscene.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cutscene : MonoBehaviour
+{
+    [TextArea(4, 30)]
+    public string[] page;
+    int pageNumber = -1;
+    Text view;
+
+
+    void Start()
+    {
+        view = GetComponent<Text>();
+        Invoke("PageTurn", 2);
+    }
+
+
+    //void Update()
+    //{
+    //    if (pageNumber < 0) view.text = "";
+    //    if (Input.GetButtonDown("Submit"))
+    //}
+
+    void PageTurn ()
+    {
+        pageNumber++;
+        if(pageNumber < page.Length)view.text = page[pageNumber];
+        Invoke("PageTurn", 2);
+    }
+
+}
