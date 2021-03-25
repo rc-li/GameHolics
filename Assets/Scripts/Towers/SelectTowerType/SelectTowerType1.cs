2db024415aabf71b9bfa097ce174309a6d778857
Sheng Luo <luos5@uci.edu>
2021-03-25 15:35:09 +0800

    tower placement preview

--- Assets/Scripts/Towers/SelectTowerType/SelectTowerType1.cs
+++ Assets/Scripts/Towers/SelectTowerType/SelectTowerType1.cs
@@ -6,11 +6,14 @@ public class SelectTowerType1 : MonoBehaviour
 {
 	public GameObject towerPrefab;
     public static int towerPrice = 30;
+    private Sprite sprite;
+    private Hover hover;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //hover = GameObject.Find("Hover").GetComponent<Hover>();
+        //sprite = gameObject.GetComponent<SpriteRenderer>().sprite;
     }
 
     // Update is called once per frame
@@ -19,14 +22,27 @@ public class SelectTowerType1 : MonoBehaviour
         
     }
 
+    // public Sprite Sprite
+    // {
+    //     get
+    //     {
+    //         return sprite;
+    //     }
+    // }
+
     public void Click()
     {
         Debug.Log ("Button Clicked");
+        
     }
     public void OnMouseUp()
 	{
         PlayerStatus.towerPrefab = towerPrefab;
         PlayerStatus.selectTowerNumber = 1;
+        sprite = gameObject.GetComponent<SpriteRenderer>().sprite;
+        hover = GameObject.Find("Hover").GetComponent<Hover>();
+        Debug.Log(sprite);
+        hover.Activate(sprite);
         //Debug.Log("Type 1 tower selected!!!");
 	}
 }
