cd0e7e7f78f2660bd2a6bd264493cec7041e2416
ShengLuo <luos5@uci.edu>
2021-03-30 21:57:02 +0800

    stored volume into memory

--- Assets/Scripts/UI/MusicPlayerScript.cs
+++ Assets/Scripts/UI/MusicPlayerScript.cs
@@ -1,22 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MusicPlayerScript : MonoBehaviour
 {
     public AudioSource AudioSource;
-
+    public Slider volumeSlider;
     private float musicVolume = 1f;
     // Start is called before the first frame update
     void Start()
     {
+        musicVolume = PlayerPrefs.GetFloat("volume");
         AudioSource.Play();
+        
+        AudioSource.volume = musicVolume;
+        volumeSlider.value = musicVolume;
     }
 
     // Update is called once per frame
     void Update()
     {
         AudioSource.volume = musicVolume;
+        PlayerPrefs.SetFloat("volume", musicVolume);
     }
 
     public void updateVolume( float volume)
