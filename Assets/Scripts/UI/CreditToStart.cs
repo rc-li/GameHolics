607f44fce90331aca1e6e1e7593b60031b6218c8
Sheng Luo <luos5@uci.edu>
2021-04-21 14:21:03 +0800

    auto switch to start menu after credits scene

---
+++ Assets/Scripts/UI/CreditToStart.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CreditToStart : MonoBehaviour
+{
+    public SceneFader sceneFader;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadMenu",31);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadMenu()
+    {
+        sceneFader.FadeToScene("StartMenu");
+    }
+}
